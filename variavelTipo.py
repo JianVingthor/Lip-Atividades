18b4f27dd9f93da6e4d82092d7985018d317aa3f
Jian Vingthor <jianvingthor@gmail.com>
2025-08-25 11:45:24 -0300

    Add files via upload
    um programa uma variável para cada tipo:
    • int
    • float
    • bool
    • str
    • Imprima na tela o valor, o id e o tipo de cada variável

---
+++ variavelTipo.py
@@ -0,0 +1,9 @@
+inteiro = 5
+ponto = 2.5
+booleano = True
+str = "Meu nome é tchau"
+
+print(f'\n tipo de variavel: {type(inteiro)} \n id da variavel: {id(inteiro)} \n valor da variavel: {inteiro}')
+print(f'\n tipo de variavel: {type(ponto)} \n id da variavel: {id(ponto)} \n valor da variavel: {ponto}')
+print(f'\n tipo de variavel: {type(booleano)} \n id da variavel: {id(booleano)} \n valor da variavel: {booleano}')
+print(f'\n tipo de variavel: {type(str)} \n id da variavel: {id(str)} \n valor da variavel: {str}')
